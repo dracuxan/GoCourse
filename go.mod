191d238aa4ecb2e8d5b5489cef199c836c77b91d
Nisarg2061 <khodkenisarg@gmail.com>
2024-08-23 22:04:43 +0530

    Completed Homework 1

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/RedHatOfficial/GoCourse
 
 go 1.17
+
+require (
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/tools v0.24.0 // indirect
+)
--- homeworks/factorial1/factorial.go
+++ homeworks/factorial1/factorial.go
@@ -1,8 +1,14 @@
 package main
 
 func factorial(n int64) int64 {
-	// enter your implementation there
-	// calculation:
-	// 1 for n <= 0
-	// n! for n >= 1
+	if n <= 1 {
+		return 1
+	} else {
+		var fact int64 = 1
+		var i int64
+		for i = 1; i <= n; i++ {
+			fact = fact * i
+		}
+		return fact
+	}
 }
